347e7d2a7513cef00fda3af9095f67138a5cb74e
Nadine2016 <nspies@us.ibm.com>
2020-04-16 14:51:29 -0400

    Update db.tf

--- db.tf
+++ db.tf
@@ -6,7 +6,7 @@ resource "ibm_database" "database" {
   name              = "tfdb"
   plan              = "standard"
   location          = "us-south"
-  resource_group_id = "${data.ibm_resource_group.resource_group.id}
+  resource_group_id = "${data.ibm_resource_group.resource_group.id}"
 
   service = "databases-for-postgresql"
   version = 11
@@ -21,7 +21,7 @@ resource "ibm_database" "database" {
 resource "ibm_resource_key" "resourceKey" {
   name                 = "tfkey"
   role                 = "Viewer"
-  resource_instance_id = "${ibm_database.database.id}
+  resource_instance_id = "${ibm_database.database.id}"
   
   parameters {
 
